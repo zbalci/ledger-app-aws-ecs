bcf4fa216d9f178c65f696c6c56a8355ad477f2d
Zeki Balcı <zkbalci@gmail.com>
2026-01-20 03:06:08 +0000

    app/web port change to 80

--- web/app/app.py
+++ web/app/app.py
@@ -67,4 +67,4 @@ def create_app():
 
 # # Run the Flask app
 # if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=8081, debug=True)
+    # app.run(host="0.0.0.0", port=80, debug=True)
--- web/app/run.py
+++ web/app/run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
