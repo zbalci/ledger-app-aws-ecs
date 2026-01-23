59843ae0007c16f591d84a8498c62f44f6212d5a
Zeki <zkbalci@gmail.com>
2026-01-23 23:57:32 +0300

    template-url changed

--- aws_cf_create.sh
+++ aws_cf_create.sh
@@ -1,6 +1,6 @@
 aws cloudformation create-stack \
-  --stack-name FullStackRolling \
-  --template-url https://ledger-app-aws-ecs-iac.s3.eu-central-1.amazonaws.com/0-main-stack.yaml \
+  --stack-name BlueGreenStack \
+  --template-url https://ledger-app-aws-iac.s3.eu-north-1.amazonaws.com/0-main-stack.yaml \
   --disable-rollback \
   --capabilities CAPABILITY_IAM \
   --capabilities CAPABILITY_NAMED_IAM
