ea648375124179928c4de3295f1bb99580835857
Zeki <zkbalci@gmail.com>
2026-01-25 06:14:13 +0300

    cloudformation file structer changed

--- aws_cf_create.sh
+++ aws_cf_create.sh
@@ -1,6 +1,6 @@
 aws cloudformation create-stack \
-  --stack-name BlueGreenStack \
-  --template-url https://ledger-app-aws-iac.s3.eu-north-1.amazonaws.com/0-main-stack.yaml \
+  --stack-name RollingStack \
+  --template-url https://ledger-app-aws-iac.s3.eu-north-1.amazonaws.com/root.yaml \
   --disable-rollback \
   --capabilities CAPABILITY_IAM \
   --capabilities CAPABILITY_NAMED_IAM
