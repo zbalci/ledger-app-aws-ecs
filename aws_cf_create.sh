7c0610d964ed7f95b07e640df439aadb4377afa9
Zeki <zkbalci@gmail.com>
2026-01-30 06:20:26 +0300

    code and deployment files restructured

--- aws_cf_create.sh
+++
@@ -1,8 +0,0 @@
-aws cloudformation create-stack \
-  --stack-name RollingStack \
-  --template-url https://ledger-app-aws-iac.s3.eu-north-1.amazonaws.com/root.yaml \
-  --disable-rollback \
-  --capabilities CAPABILITY_IAM \
-  --capabilities CAPABILITY_NAMED_IAM
-
-aws cloudformation create-stack --stack-name RollingStack --capabilities CAPABILITY_IAM --capabilities CAPABILITY_NAMED_IAM --template-url https://ledger-app-aws-iac.s3.eu-north-1.amazonaws.com/root.yaml --disable-rollback
---
+++ infrastructure/deploy.sh
@@ -0,0 +1,13 @@
+aws cloudformation create-stack \
+  --stack-name ledger-prod-foundation \
+  --template-url https://ledger-app-aws-iac.s3.eu-north-1.amazonaws.com/foundation/root.yaml \
+  --disable-rollback \
+  --capabilities CAPABILITY_IAM \
+  --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation create-stack \
+  --stack-name ledger-prod-root \
+  --template-url https://ledger-app-aws-iac.s3.eu-north-1.amazonaws.com/root.yaml \
+  --disable-rollback \
+  --capabilities CAPABILITY_IAM \
+  --capabilities CAPABILITY_NAMED_IAM
